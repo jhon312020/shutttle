8ba733d664026b1f8dd4320390c1f1e3fe5ad82d
Velu <velmurugan@proisc.com>
2017-02-15 19:03:23 +0530

    Fixed carousel css for fade-in transition

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/shutttle';
+$config['base_url'] = preg_replace('/\/index.php.*/', '', $_SERVER['PHP_SELF']);
 
 /*
 |--------------------------------------------------------------------------
--- application/modules/layout/views/templates/slider.php
+++ application/modules/layout/views/templates/slider.php
@@ -4,7 +4,7 @@
 ?>
 <!-- Carousel
     ================================================== -->
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="2500">
       <!-- Indicators -->
       <!-- <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
