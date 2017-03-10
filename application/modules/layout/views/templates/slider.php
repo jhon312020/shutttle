e257d96e6741ca6de553548ceda498f79bed543e
aditya14 <adityaramadas@gmail.com>
2017-03-10 11:27:26 -0500

    Slider changes

--- application/modules/layout/views/templates/slider.php
+++ application/modules/layout/views/templates/slider.php
@@ -5,41 +5,33 @@
 <!-- Carousel
     ================================================== -->
     <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="2500">
-      <!-- Indicators -->
-      <!-- <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1" class=""></li>
-        <li data-target="#myCarousel" data-slide-to="2" class=""></li>
-      </ol> -->
-      <div class="carousel-inner" role="listbox">
-        <?php
-          $count = 0;
-          $active_class = 'active';
-          foreach ($sliders as $slider) {
-            $count++;
-            
-            if ($ln == 'es') {
-              $slogan = $slider->slogan_es;
-            } else {
-              $slogan = $slider->slogan_en;
-            } ?>
-        <div class="item <?php echo $active_class; ?>">
-          <img class="slide-<?php echo $count;?>" src="<?php echo IMAGEPATH.'homepage/slider/'.$slider->image;?>" alt="Slide <?php echo $count;?>">
-          <div class="container">
-            <div class="carousel-caption">
-              <p><?php echo $slogan; ?></p>
-              <p><a class="btn btn-lg" href="<?php echo site_url($ln."/reservation"); ?>" role="button">BOOK NOW</a></p>
+        <div class="carousel-inner" role="listbox">     
+            <div class="item active">
+                <div id="csl-item1" class="carousel-image">&nbsp;</div>
+                <div class="container">
+                    <div class="carousel-caption">
+                        <p>SLOGAN 1</p>
+                        <p><a class="btn btn-lg" href="<?php echo site_url($ln."/reservation"); ?>" role="button">BOOK NOW</a></p>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div class="item">
+                <div id="csl-item2" class="carousel-image">&nbsp;</div>
+                <div class="container">
+                    <div class="carousel-caption">
+                        <p>SLOGAN 2</p>
+                        <p><a class="btn btn-lg" href="<?php echo site_url($ln."/reservation"); ?>" role="button">BOOK NOW</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div id="csl-item3" class="carousel-image">&nbsp;</div>
+                <div class="container">
+                    <div class="carousel-caption">
+                        <p>SLOGAN 3</p>
+                        <p><a class="btn btn-lg" href="<?php echo site_url($ln."/reservation"); ?>" role="button">BOOK NOW</a></p>
+                    </div>
+                </div>
+            </div> 
         </div>
-        <?php $active_class = ''; } ?>
-      </div>
-      <!-- <a class="left carousel-control" href="#" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="right carousel-control" href="#" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span> -->
-      </a>
-    </div><!-- /.carousel -->
+    </div>
