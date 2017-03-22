361a8e10945fbc2edb0df59efdd15bb7b47fcd59
megamind <janakiraman@proisc.com>
2017-03-22 22:13:08 +0530

    Updated the following file to fix design issue of slider.

--- application/modules/layout/views/templates/slider.php
+++ application/modules/layout/views/templates/slider.php
@@ -5,16 +5,31 @@
 ?>
 <!-- Carousel
     ================================================== -->
-    <div id="myCarousel" class="carousel slide carousel-fade">
-        <div class="carousel-inner" role="listbox">     
-            <div class="item active">
+    <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="2500">
+        <div class="carousel-inner" role="listbox">
+        <?php
+            $count = 0;
+            $active_class = 'active';
+            foreach ($sliders as $slider) {
+            $count++;
+            
+            if ($ln == 'es') {
+              $slogan = $slider->slogan_es;
+            } else {
+              $slogan = $slider->slogan_en;
+            }
+
+            ?>     
+            <div class="item <?php echo $active_class; ?>">
+                <div style="background:url(<?php echo IMAGEPATH.'homepage/slider/'.$slider->image;?>);background-repeat: no-repeat;background-position: center top;background-size: cover;" class="carousel-image cs-item">&nbsp;</div>
                 <div class="container" style="height:100vh;">
                     <div class="sliderSection">
-						<img src="<?php echo IMAGEPATH.'header/sliders/1.png' ?>" />
-                        <p class="sliderSubtitle">The best shuttle between the airport the the city</p>
-                        <p><a class="btn btn-lg" href="" role="button">BOOK NOW</a></p>
+                        <img src="<?php echo IMAGEPATH.'header/sliders/'.$count.".png" ?>" />
+                        <p class="sliderSubtitle"><?php echo $slogan; ?></p>
+                        <p><a class="btn btn-lg" href="<?php echo site_url($ln."/reservation"); ?>" role="button"><?php echo lang('book_now'); ?></a></p>
                     </div>
                 </div>
             </div>
+            <?php $active_class = ''; } ?>
         </div>
     </div>
